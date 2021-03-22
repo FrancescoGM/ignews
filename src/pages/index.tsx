4c0f72d46658bc339ba8f1e14c9afeb1dc80a53c
Francesco <francescogmuller@hotmail.com>
2021-03-22 10:55:43 -0300

    init the project and add the requires librares

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <div>Hello World</div>
+    </>
+  )
+}
